2cafdaa464efe815efefa0395145d19a1543cbfe
Zachary Belford <belfordz66@gmail.com>
2020-08-19 14:39:21 -0700

    feat: nesting

--- src/index.test.ts
+++ src/index.test.ts
@@ -2,4 +2,31 @@ import validator, { ValidationErrors } from "./";
 
 describe("validator", () => {
   it("is a function", () => { expect(typeof validator).toBe("function"); });
+
+  it("can handle mixed schema", () => {
+    const testSchema = {
+      type: "object",
+      properties: {
+        foo: { type: "string", enum: ["abc", "xyz"] },
+        bar: {
+          type: "array",
+          items: { type: "number" }
+        }
+      }
+    };
+
+    const result0 = validator(testSchema, {
+      foo: "abc",
+      bar: [123]
+    });
+
+    expect(result0).toBe(true);
+
+    const result1 = validator(testSchema, {
+      foo: "wrong",
+      bar: [123]
+    });
+
+    expect(result1).toBeInstanceOf(ValidationErrors);
+  });
 });
